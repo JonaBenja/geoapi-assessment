a0c9de24f9a91e22caa61acd85dc67877c3b6389
JonaBenja <47357431+JonaBenja@users.noreply.github.com>
2025-06-24 12:14:46 +0200

    Added more tests for locations/ endpoint

--- src/assessment/geoapi/tests.py
+++ src/assessment/geoapi/tests.py
@@ -16,7 +16,7 @@ class GeoLocationAPITests(APITestCase):
     @classmethod
     def setUpTestData(cls):
 
-        # Create 3 different test users
+        # Create 2 different test users
         cls.user_1 = User.objects.create_user(
             username="user_1", password="secret123", email="test@example.com"
         )
@@ -25,20 +25,44 @@ class GeoLocationAPITests(APITestCase):
             username="user_2", password="secret123", email="test@example.com"
         )
 
-        cls.user_3 = User.objects.create_user(
-            username="user_3", password="secret123", email="test@example.com"
-        )
-
         # For each user, create a GeoLocation instance
         cls.geo_1 = GeoLocationFactory(user=cls.user_1)
         cls.geo_2 = GeoLocationFactory(user=cls.user_2)
-        cls.geo_3 = GeoLocationFactory(user=cls.user_3)
 
-    def test_list_locations(self):
+    def test_list_locations_unauthenticated(self):
+        url = reverse("location-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_locations_authenticated(self):
+        self.client.force_authenticate(user=self.user_1)
         url = reverse("location-list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data["results"]), 3)
+
+    def test_list_only_locations_created_by_user(self):
+        url = reverse("location-list")
+
+        # First get all locations for user 1
+        self.client.force_authenticate(user=self.user_1)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Make sure user 1 is the user of all locations
+        results_user_1 = response.data["results"]
+        self.assertTrue(all(item["user"] == "user_1" for item in results_user_1))
+
+        # Then get all locations for user 2
+        self.client.force_authenticate(user=self.user_2)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Make sure user 2 is the user of all locations
+        results_user_2 = response.data["results"]
+        self.assertTrue(all(item["user"] == "user_2" for item in results_user_2))
+
+        # Make sure locations of user 1 are different from locations of user 2
+        self.assertNotEqual(results_user_1, results_user_2)
 
     def test_create_location_unauthenticated(self):
         url = reverse("location-list")
--- src/assessment/geoapi/views.py
+++ src/assessment/geoapi/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
 from .models import GeoLocation
@@ -8,7 +8,12 @@ from .serializers import GeoLocationSerializer
 class GeoLocationList(generics.ListCreateAPIView):
     queryset = GeoLocation.objects.all()
     serializer_class = GeoLocationSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
+    # Save user that created the instance
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    # Only return locations created by current user
+    def get_queryset(self):
+        return GeoLocation.objects.filter(user=self.request.user)
