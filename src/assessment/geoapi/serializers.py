2f7d8718211fb3a65b18f6a86f55dd3137957191
JonaBenja <47357431+JonaBenja@users.noreply.github.com>
2025-06-24 11:24:22 +0200

    Let users POST lat/long data
    Lat/long data is validated and then transformed into a Point()
    object, which is saved in the newly created model instance.

--- src/assessment/geoapi/serializers.py
+++ src/assessment/geoapi/serializers.py
@@ -1,10 +1,45 @@
+from django.contrib.gis.geos import Point
+
 from rest_framework import serializers
+
 from .models import GeoLocation
 
 
 class GeoLocationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the GeoLocation model.
+
+    Allows creation of GeoLocation points from latitude and longitude inputs.
+    Includes validation of the latitude and longitude inputs.
+    Creates a Point() object from latitude and longitude.
+    """
+
     user = serializers.StringRelatedField(read_only=True)
+    latitude = serializers.FloatField(write_only=True)
+    longitude = serializers.FloatField(write_only=True)
 
     class Meta:
         model = GeoLocation
-        fields = ["location", "timestamp", "user"]
+        fields = ["id", "latitude", "longitude", "location", "timestamp", "user"]
+        read_only_fields = ["location", "timestamp", "user"]
+
+    def validate(self, data):
+        latitude = data.get("latitude")
+        longitude = data.get("longitude")
+
+        if not (-90 <= latitude <= 90):
+            raise serializers.ValidationError(
+                "Latitude needs to be between -90 and 90."
+            )
+        if not (-180 <= longitude <= 180):
+            raise serializers.ValidationError(
+                "Longitude needs to be between -180 and 180."
+            )
+
+        return data
+
+    def create(self, validated_data):
+        longitude = validated_data.pop("longitude")
+        latitude = validated_data.pop("latitude")
+        point = Point(longitude, latitude)
+        return GeoLocation.objects.create(location=point, **validated_data)
