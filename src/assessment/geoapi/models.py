a3d333db519de86479886c9d8c0a9716c9682679
JonaBenja <47357431+JonaBenja@users.noreply.github.com>
2025-06-24 10:18:36 +0200

    Create GeoLocation model and migration

--- src/assessment/geoapi/models.py
+++ src/assessment/geoapi/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class GeoLocation(models.Model):
+    location = models.PointField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.user or 'Mystery'} at {self.location}"
